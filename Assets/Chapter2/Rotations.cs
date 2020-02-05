d92dde50291c9b236ec215de48e395ee0dd7f3c8
Shiva Teja Chigicherla <shiva000teja@gmail.com>
2020-02-05 23:13:46 +0530

    c2

---
+++ Assets/Chapter2/Rotations.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotations : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cube;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Instantiate(cube, Vector3.zero, Quaternion.identity);
+            Instantiate(cube, Vector3.zero, Quaternion.EulerAngles(10,0,0));
+
+        }
+    }
+}
